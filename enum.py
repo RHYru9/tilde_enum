e7f4cd50b19a7e52b9c3f12c09189fbaaeb71cc5
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2025-01-24 11:06:15 +0700

    Update enum.py

--- enum.py
+++ enum.py
@@ -29,7 +29,7 @@ from lib.getTerminalSize import getTerminalSize
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-headers = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36'}
 methods = ["GET","POST","OPTIONS","HEAD","TRACE","TRACK","DEBUG"]
 tails = ["\\a.asp","/a.asp","\\a.aspx","/a.aspx","/a.shtml","/a.asmx","/a.ashx","/a.config","/a.php","/a.jpg","","/a.xxx"]
 targets = []
